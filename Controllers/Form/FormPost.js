229a4cc047a96add89409bb6096b84b112cc88a0
Douglas Pujol <douglas.pujol@outlook.com>
2022-08-02 22:06:56 -0300

    Page Login

--- Controllers/Form/FormPost.js
+++ Controllers/Form/FormPost.js
@@ -1,6 +1,4 @@
-/*
 export class FormPost {
-
   constructor(idForm, idTextarea, ListPost) {
     this.form = document.getElementById(idForm);
     this.textarea = document.getElementById(idTextarea);
@@ -9,26 +7,27 @@ export class FormPost {
   }
 
   formValidate(value) {
-    if (value === '' || value === null || value === undefined || value.length < 3) {
-      return false
+    if (
+      value === "" ||
+      value === null ||
+      value === undefined ||
+      value.length < 3
+    ) {
+      return false;
     }
-    return true
+    return true;
   }
-
 
   onSubmit(func) {
-    this.form.addEventListener("submit", func)
+    this.form.addEventListener("submit", func);
   }
-
-
 
   addSubmit() {
     const handleSubmit = (event) => {
-
       event.preventDefault();
       if (this.formValidate(this.textarea.value)) {
         const newPost = document.createElement("li");
-        newPost.classList.add('post')
+        newPost.classList.add("post");
         const time = this.getTime();
         newPost.innerHTML = `
         <div class="infoUserPost">
@@ -61,11 +60,10 @@ export class FormPost {
         `;
         this.listPost.appendChild(newPost);
         this.textarea.value = "";
-
       } else {
-        alert('ALGUM ERRO');
+        alert("ALGUM ERRO");
       }
-    }
+    };
 
     this.onSubmit(handleSubmit);
   }
@@ -74,53 +72,8 @@ export class FormPost {
     const time = new Date();
     const hour = time.getHours();
     const minutes = time.getMinutes();
-    return `${hour}h ${minutes}min`
+    return `${hour}h ${minutes}min`;
   }
-
 }
 
-const postForm = new FormPost('formPost', 'textarea', 'posts')
- */
-function sumAllEven(list) {
-  let tamanho = list.length;
-  let resultArray = [];
-  let soma = 0;
-
-  for (var i = 0; i < tamanho; i++) {
-    let valor = list[i];
-
-
-
-    if (valor.constructor.name == "Array") {
-      let tamanho2 = valor.length;
-      var somaArray = 0;
-
-      for (var d = 0; d < tamanho2; d++) {
-        let valor2 = valor[d]
-        if (isNaN(valor2) || valor2 == null || valor2 == undefined) { continue }
-        if (!(valor2 % 2) == 0) { continue }
-        valor2 = parseInt(valor2);
-        somaArray += valor2
-      }
-      soma += somaArray;
-      continue
-    }
-
-    if (isNaN(valor) || valor == null || valor == undefined) { continue }
-
-
-    //REMOVE IMPAR
-    if (!(valor % 2) == 0) { continue }
-
-    valor = parseInt(valor);
-
-    resultArray.push(valor);
-
-    soma += valor
-  }
-  return soma
-}
-
-
-const teste = sumAllEven([[2, 0, null, 'undeifas'], 30, 20, 15, 'jon', [5, 5, 5, 2]])
-console.log(teste);
+const postForm = new FormPost("formPost", "textarea", "posts");
